5ad0675e21c1d3293cc1041486d86b5e3f6071b1
nehaPandit96 <npandit165@gmail.com>
2024-02-06 09:20:30 -0500

    Devops assignment -3

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const SERVER_PORT = process.env.PORT || 3000;
+
+// Route for the Home page
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// Route for the Products page
+app.get("/products", (req, res) => {
+  res.sendFile(__dirname + "/views/products.html");
+});
+
+// Start the server
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+});
